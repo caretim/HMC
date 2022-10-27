f4c23ee1650b24395ac9e1560139ad28bca06564
caretim <karesin72041@gmail.com>
2022-10-27 14:36:52 +0900

    accounts/login,update

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('update/',views.update,name="update"),
+    path('login/',views.login,name="login"),
     
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as my_login, logout as my_logout, get_user_model
 
 # Create your views here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def login (request):
+    if request.method == 'post':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            my_login(request,form.get_user())
+            return redirect(" article:index ")
+    else:
+        form = AuthenticationForm(request.POST)
+    context = {
+        'form':form
+    }
+    return render (request, "accounts/login.html",context)
+
+
+def update (request,pk):
+    if request.method == 'POST':
+        user =get_user_model().objects.get(pk=pk)
+        if request.user == user.user:
+            form = AuthenticationForm(request.POST ,instance=user)
+            form.is_valid()
+            form.save()
+            return redirect("article:index")
+    else:
+        form = AuthenticationForm(instance=user)
+    context={
+        'form':form
+    }
+    return render (request,"accounts/update.html",context)
