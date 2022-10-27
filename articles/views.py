818c43145ca6255c55e926292afb46a1f93594aa
wnsghd14 <wnsghd14@gmail.com>
2022-10-27 16:25:27 +0900

    add_index

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from .models import Article
+from django.views.decorators.http import require_safe
+from django.core.paginator import Paginator
 # Create your views here.
+
+@require_safe
 def index(request):
-    return render(request, 'articles/index.html')
+    page = request.GET.get('page','1')
+    articles = Article.objects.order_by('-pk')
+    paginator = Paginator(articles,10)
+    page_obj = paginator.get_page(page)
+    context = {
+        'articles': page_obj
+    }
+    return render(request, 'articles/index.html', context)
