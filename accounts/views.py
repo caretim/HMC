8011bc3d53e00aa672ebe7313995d4024fb11139
tenedict <tenedict@gmail.com>
2022-10-28 16:06:12 +0900

    tier성별구분 완료

--- accounts/views.py
+++ accounts/views.py
@@ -36,27 +36,50 @@ def detail(request, pk):
     user = get_user_model().objects.get(pk=pk)
     weight = user.userweights
     tall = user.usertall
-    fat = (weight*user.userfat)/100
-    standard_w = ((tall/100)**2)*22
-    standard_nf = standard_w*0.85
-    standard_f = standard_w*0.15
-    score = 80 -(standard_nf-(weight-fat))+(standard_f-fat)
-    if score > 150:
-        tier = 1
-    elif score > 130 :
-        tier = 2
-    elif score > 110:
-        tier = 3
-    elif score > 90 :
-        tier = 4
-    elif score > 80 :
-        tier = 5
-    elif score > 70 :
-        tier = 6
-    elif score > 60 :
-        tier = 7
+    if user.gender == 1:
+        fat = (weight*user.userfat)/100
+        standard_w = ((tall/100)**2)*22
+        standard_nf = standard_w*0.85
+        standard_f = standard_w*0.15
+        score = 80 -(standard_nf-(weight-fat))+(standard_f-fat)
+        if score > 150:
+            tier = 1
+        elif score > 130 :
+            tier = 2
+        elif score > 110:
+            tier = 3
+        elif score > 90 :
+            tier = 4
+        elif score > 80 :
+            tier = 5
+        elif score > 70 :
+            tier = 6
+        elif score > 60 :
+            tier = 7
+        else:
+            tier = 8
     else:
-        tier = 8
+        fat = (weight*user.userfat)/100
+        standard_w = ((tall/100)**2)*22
+        standard_nf = standard_w*0.77
+        standard_f = standard_w*0.23
+        score = 80 -(standard_nf-(weight-fat))+(standard_f-fat)
+        if score > 150:
+            tier = 1
+        elif score > 130 :
+            tier = 2
+        elif score > 110:
+            tier = 3
+        elif score > 90 :
+            tier = 4
+        elif score > 80 :
+            tier = 5
+        elif score > 70 :
+            tier = 6
+        elif score > 60 :
+            tier = 7
+        else:
+            tier = 8
     context = { 
             "user": user,
             "score": score,
