24da4e5d10fa23fcce838d0f3ab0617b02c46448
tenedict <tenedict@gmail.com>
2022-10-28 15:32:35 +0900

    tier 수식

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,12 +5,13 @@ from django.contrib.auth import get_user_model
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ["username", "usertall", "userweights", "userfat", "user_sm"]
+        fields = ["username", "usertall", "userweights", "userfat", "user_sm","gender"]
         labels = {
             "usertall": "키",
             "userweights": "몸무게",
             "userfat": "체지방량",
             "user_sm": "골격근량",
+            "gender": "성별",
         }
 
 
